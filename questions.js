b4c259c2acbb92294b3d780df9c1cb8a39e379ad
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2021-04-25 08:55:36 +0700

    Add more questions.js

--- questions.js
+++ questions.js
@@ -2,11 +2,11 @@ var questions = [{ question: "Print Hello world", answer: 'print("Hello world")'
 { level: 1, question: "Define a list named friends to store 3 friends: Nam, Hoa, Tu", answer: 'friends = ["Nam", "Hoa", "Tu"]' },
 { level: 1, question: "Define a function to print Hello World",answer: 'def hello():#^print("Hello world")'},
 { level: 1, question: "Define a function to print Bye Bye",answer: 'def bye():#^print("Bye Bye")'},
-{ level: 2, question: "Define a function to print I Love Daddy",answer: 'def love():#^print("I Love Daddy")'},
-{ level: 2, question: "Define a function to print I Love Mom",answer: 'def loveMom():#^print("I Love Mom")'},
-{ level: 3, question: "Define a list named countries to store 3 countries: Vietnam, US, UK", answer: 'friends = ["Vietnam", "US", "UK"]' },
-{ level: 3, question: "Define a function add(a, b) to add 2 number ", answer: 'def add(a, b):#^c = a + b#^return c' }
+{ level: 1, question: "Define a function to print I Love Daddy",answer: 'def love():#^print("I Love Daddy")'},
+{ level: 1, question: "Define a function to print I Love Mom",answer: 'def loveMom():#^print("I Love Mom")'},
+{ level: 1, question: "Define a list named countries to store 3 countries: Vietnam, US, UK", answer: 'friends = ["Vietnam", "US", "UK"]' },
+{ level: 1, question: "Define a function add(a, b) to add 2 number ", answer: 'def add(a, b):#^c = a + b#^return c' }
 
 ]
 
-questions.push({ level: 3, question: "calculate 7 + 5:",answer: "a = 7 + 5"})
+
---
+++ questions2.js
@@ -0,0 +1,6 @@
+questions.push({ level: 2, question: "calculate 7 + 5:",answer: "a = 7 + 5"})
+questions.push({ level: 2, question: "calculate 7 + 5:",answer: "a = 7 + 5"})
+questions.push({ level: 2, question: "calculate 7 + 5:",answer: "a = 7 + 5"})
+questions.push({ level: 2, question: "calculate 7 + 5:",answer: "a = 7 + 5"})
+questions.push({ level: 2, question: "calculate 7 + 5:",answer: "a = 7 + 5"})
+questions.push({ level: 2, question: "calculate 7 + 5:",answer: "a = 7 + 5"})
